5fe28b5659b39693fae6898c767c8d53e343ce63
Unreal-Vikhyat <vikhyatsharma137@gmail.com>
2025-11-01 11:03:15 +0530

    changes for deploying on render

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => console.log("User disconnected"));
 });
 
-const PORT = 3000;
-server.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
